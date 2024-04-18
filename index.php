67849b22ff0110c7634b898c9a3391331f7ecd70
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-04-18 10:32:28 +0600

    con

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
+
 <body>
     <h1>To Do List</h1>
     <div class="container">
@@ -33,4 +35,5 @@
         </form>
     </div>
 </body>
+
 </html>
